3bc9a6e92868aa9b0f25c63cf485c6e55a00b318
Jason Slagle <jslagle@gmail.com>
2012-08-23 15:57:13 -0400

    Working module - needs a bit more work - spec tests for param validation aren't right, and sshd_config likely needs some defaults passed for file location

--- manifests/server.pp
+++ manifests/server.pp
@@ -1,5 +1,16 @@
-class ssh::server($ensure = 'present', $enabled = true,
-                  $svcensure = 'present') {
+class ssh::server($ensure = 'present',
+  $enabled = true,
+  $svcensure = 'present',
+  $permitroot = 'No',
+  $aliveinterval = '60',
+  $alivecount = '3',
+  $privilegeseperation = 'Yes',
+  $maxauth = '2',
+  $passwordauth = 'No',
+  $usepam = 'No',
+  $kerberosauth = 'No',
+  $allowusers = ""
+) {
   include ssh::params
 
   if ($ssh::params::serverpkg != undef) {
@@ -9,11 +20,106 @@ class ssh::server($ensure = 'present', $enabled = true,
     }
   }
 
+  # Validate Parameters
+  if ($permitroot !~ /(?i:yes|no|without-password|forced-commands-only)/) {
+    fail("permitroot must be one of yes, no, without-password or forced-commands-only")
+  }
+
+  if  ($aliveinterval !~ /^\d+$/ or $aliveinterval =~ /^0+$/ ) {
+    fail("aliveinterval must be a positive number")
+  }
+
   service { 'ssh':
     ensure  => $svcensure,
     name    => $ssh::params::service,
     enable  => $enabled,
   }
+
+  # All of our sshd_config options that do not allow redefinition
+  sshd_config { 'IgnoreRhosts':
+    value  => 'No',
+    notify => Service['ssh'],
+  }
+
+  sshd_config { 'HostbasedAuthentication':
+    value => 'No',
+    notify => Service['ssh'],
+  }
+
+  sshd_config { 'PermitEmptyPasswords':
+    value => 'No',
+    notify => Service['ssh'],
+  }
+
+  sshd_config { 'LogLevel':
+    value  => 'INFO',
+    notify => Service['ssh'],
+  }
+
+  sshd_config { 'StrictModes':
+    value  => 'Yes',
+    notify => Service['ssh'],
+  }
+
+  sshd_config { 'TCPKeepAlive':
+    value  => 'No',
+    notify => Service['ssh'],
+  }
+
+  sshd_config { 'GSSAPIAuthentication':
+    value  => 'No',
+    notify => Service['ssh'],
+  }
+
+  # Params that allow changes
+  sshd_config { 'PermitRootLogin':
+    value  => $permitroot,
+    notify => Service['ssh'],
+  }
+
+  sshd_config { 'ClientAliveInterval':
+    value  => $aliveinterval,
+    notify => Service['ssh'],
+  }
+
+  sshd_config { 'ClientAliveCountMax':
+    value  => $alivecount,
+    notify => Service['ssh'],
+  }
+
+  sshd_config { 'UsePrivilegeSeperation':
+    value  => $privilegeseperation,
+    notify => Service['ssh'],
+  }
+
+  sshd_config { 'MaxAuthTries':
+    value  => $maxauth,
+    notify => Service['ssh'],
+  }
+
+  sshd_config { 'PasswordAuthentication':
+    value  => $passwordauth,
+    notify => Service['ssh'],
+  }
+
+  sshd_config { 'UsePAM':
+    value  => $usepam,
+    notify => Service['ssh'],
+  }
+
+  sshd_config { 'KerberosAuthentication':
+    value  => $kerberosauth,
+    notify => Service['ssh'],
+  }
+
+  # Handle AllowUsers
+  if ($allowusers != "") {
+    sshd_config { 'AllowUsers':
+      value => $allowusers,
+      notify => Service['ssh'],
+    }
+  }
+
   if ($ssh::params::serverpkg != undef) {
     Package['ssh-server'] -> Service['ssh']
   }
--- spec/classes/ssh_server_spec.rb
+++ spec/classes/ssh_server_spec.rb
@@ -1,4 +1,6 @@
 #!/usr/bin/env rspec
+$:.unshift File.join(File.dirname(__FILE__), '..', 'fixtures/modules/augeasproviders/lib')
+
 require 'spec_helper'
 
 describe 'ssh::server' do
@@ -10,7 +12,7 @@ describe 'ssh::server' do
     let(:facts) { { :osfamily => 'RedHat' }}
 
     describe 'RedHat should get openssh-server package' do
-      it { should contain_package('openssh-server') }
+      it { should contain_package('openssh-server').with_ensure('present') }
     end
 
   end
@@ -19,7 +21,7 @@ describe 'ssh::server' do
     let(:facts) { { :osfamily => 'Debian' }}
 
     describe 'Debian uses ssh-server package' do
-      it { should contain_package('ssh-server') }
+      it { should contain_package('ssh-server').with_ensure('present') }
     end
 
   end
@@ -27,6 +29,168 @@ describe 'ssh::server' do
   describe 'Non OS specific' do
     describe 'ssh service should be running' do
       it { should contain_service('ssh') }
+    end
+  end
+
+  describe 'SSH Option checks' do
+    describe 'Should not allow rhosts' do
+      it { should contain_sshd_config('IgnoreRhosts').with('value' => 'No') }
+    end
+    describe 'Should not allow host based auth' do
+      it { should contain_sshd_config('HostbasedAuthentication').with('value' => 'No') }
+    end
+    describe 'Not permit root login by default' do
+      it { should contain_sshd_config('PermitRootLogin').with('value' => 'No') }
+    end
+    describe 'Not permit empty password' do
+      it { should contain_sshd_config('PermitEmptyPasswords').with('value' => 'No') }
+    end
+    describe 'Set the loglevel to INFO' do
+      it { should contain_sshd_config('LogLevel').with('value' => 'INFO') }
+    end
+    describe 'Enforce strict modes' do
+      it { should contain_sshd_config('StrictModes').with('value' => 'Yes') }
+    end
+    describe 'Should disable TCPKeepAlive' do
+      it { should contain_sshd_config('TCPKeepAlive').with('value' => 'No') }
+    end
+    describe 'Should enable ClientAliveInterval with a default of 60 seconds' do
+      it { should contain_sshd_config('ClientAliveInterval').with('value' => '60') }
+    end
+    describe 'Should set the ClientAliveCountMax to 3 by default' do
+      it { should contain_sshd_config('ClientAliveCountMax').with('value' => '3') }
+    end
+    describe 'Should enable Privilege seperation by default' do
+      it { should contain_sshd_config('UsePrivilegeSeperation').with('value' => 'Yes') }
+    end
+    describe 'Should set MaxAuthTries to 2 by default' do
+      it { should contain_sshd_config('MaxAuthTries').with('value' => '2') }
+    end
+    describe 'Disable Password auth by default' do
+      it { should contain_sshd_config('PasswordAuthentication').with('value' => 'No') }
+    end
+    describe 'Should disable PAM by default' do
+      it { should contain_sshd_config('UsePAM').with('value' => 'No') }
+    end
+    describe 'Should disable Kerberos by default' do
+      it { should contain_sshd_config('KerberosAuthentication').with('value' => 'No') }
+    end
+    describe 'Disable GSSAPI Authentication' do
+      it { should contain_sshd_config('GSSAPIAuthentication').with('value' => 'No') }
+    end
+    describe 'Not have any allowed users by default' do
+      it { should_not contain_sshd_config('AllowUsers') }
+    end
+  end
+  
+  describe 'Non-Default values for config options' do
+    let(:params) { {
+      :permitroot => 'Yes',
+      :aliveinterval => '600',
+      :alivecount => '5',
+      :privilegeseperation => 'No',
+      :maxauth => '5',
+      :passwordauth => 'Yes',
+      :usepam => 'Yes',
+      :kerberosauth => 'Yes'
+    } }
+    describe 'Allow Root Login if specified' do
+      it { should contain_sshd_config('PermitRootLogin').with('value' => 'Yes') }
+    end
+    describe 'Allow a non-default numeric ClientAliveInterval' do
+      it { should contain_sshd_config('ClientAliveInterval').with('value' => '600') }
+    end
+    describe 'Allow a non-default numeric ClientAliveCountMax' do
+      it { should contain_sshd_config('ClientAliveCountMax').with('value' => '5') }
+    end
+    describe 'Allow setting PrivilegeSeperation to no' do
+      it { should contain_sshd_config('UsePrivilegeSeperation').with('value' => 'No') }
+    end
+    describe 'Allow us to change MaxAuthRetries' do
+      it { should contain_sshd_config('MaxAuthTries').with('value' => '5') }
+    end
+    describe 'Allow us to use password authentication' do
+      it { should contain_sshd_config('PasswordAuthentication').with('value' => 'Yes') }
+    end
+    describe 'Allow us to use PAM' do
+      it { should contain_sshd_config('UsePAM').with('value' => 'Yes') }
+    end
+    describe 'Allow use of kerberos auth' do
+      it { should contain_sshd_config('KerberosAuthentication').with('value' => 'Yes') }
+    end
+  end
+
+  describe 'Error handling for broken parameter values' do
+    describe 'Validate values for permitroot' do
+      let (:params) { { :permitroot => "Moo" } }
+      it {
+        expect {
+          should raise_error(Puppet:Error, /permitroot must be one of/)
+        }
+      }
+    end
+    describe 'aliveinterval must be a number' do
+      let (:params) { { :aliveinterval => 'Moo' } }
+      it {
+        expect {
+          should raise_error(Puppet::Error, /aliveinterval must be a positive number/)
+        }
+      }
+    end
+    describe 'alivecount must be a number' do
+      let (:params) { { :alivecount => 'Moo' } }
+      it {
+        expect {
+          should raise_error(Puppet::Error, /alivecount must be a positive number/)
+        }
+      }
+    end
+    describe 'privilegeseperation must be yes or no' do
+      let (:params) { { :privilegeseperation => 'Moo' } }
+      it {
+        expect {
+          should raise_error(Puppet::Error, /privilegeseperation must be yes or no/)
+        }
+      }
+    end
+    describe 'maxauth must be a number' do
+      let (:params) { { :maxauth => 'Moo' } }
+      it {
+        expect {
+          should raise_error(Puppet::Error, /maxauth must be a positive number/)
+        }
+      }
+    end
+    describe 'passwordauth must be yes or no' do
+      let (:params) { { :passwordauth => 'Moo' } }
+      it {
+        expect {
+          should raise_error(Puppet::Error, /passwordauth must be a yes or no/)
+        }
+      }
+    end
+    describe 'usepam must be yes or no' do
+      let (:params) { { :usepam => 'Moo' } }
+      it {
+        expect {
+          should raise_error(Puppet::Error, /usepam must be a yes or no/)
+        }
+      }
+    end
+    describe 'kerberosauth must be yes or no' do
+      let (:params) { { :kerberosauth => 'Moo' } }
+      it {
+        expect {
+          should raise_error(Puppet::Error, /kerberosauth must be a yes or no/)
+        }
+      }
+    end
+  end
+
+  describe 'Array based config items' do
+    let(:params) { { :allowusers => ["user1", "user2"] } }
+    describe 'Allow us to send in an array of permitted users' do
+      it { should contain_sshd_config('AllowUsers').with('value' => ["user1", "user2"]) }
     end
   end
 end
