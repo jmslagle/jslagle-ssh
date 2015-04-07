e2b6e9c66b996185c16ff1b791f8f5415a2e72a1
Jason Slagle <jslagle@gmail.com>
2015-04-07 11:08:36 -0400

    Add settable client options for protocol, pubkeyauthentication

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,11 +1,26 @@
-class ssh::client($ensure = 'present') {
+class ssh::client(
+  $ensure = 'present',
+  $protocol = '2',
+  $pubkeyauthentication = 'yes'
+) {
   include ssh::params
+
+  validate_re($pubkeyauthentication, ['^yes$', '^no$'])
+  validate_re($protocol, ['^1$', '^2$', '^1,2$', '^2,1$'])
 
   if ($ssh::params::clientpkg != undef) {
     package { $ssh::params::clientpkg:
       ensure => $ensure,
       name   =>  'ssh',
     }
+  }
+
+  ssh_config { 'PubkeyAuthentication':
+    value      => downcase($pubkeyauthentication),
+  }
+
+  ssh_config { 'Protocol':
+    value      => $protocol,
   }
 
 }
--- spec/classes/ssh_client_spec.rb
+++ spec/classes/ssh_client_spec.rb
@@ -24,4 +24,43 @@ describe 'ssh::client' do
       it { should contain_package('openssh-client') }
     end
   end
+  describe 'SSH Option checks' do
+    let(:facts) { { :osfamily => 'RedHat' }}
+    describe 'Should set pubkey to yes' do
+      it { should contain_ssh_config('PubkeyAuthentication').with('value' => 'yes') }
+    end
+    describe 'Should Protocol to 2' do
+      it { should contain_ssh_config('Protocol').with('value' => '2') }
+    end
+  end
+  describe 'Non-Default values for config options' do
+
+    let(:facts) { { :osfamily => 'RedHat' }}
+
+    let(:params) { {
+      :protocol => '1,2',
+      :pubkeyauthentication => 'no'
+    } }
+    describe 'Allow Protocol' do
+      it { should contain_ssh_config('Protocol').with('value' => '1,2') }
+    end
+    describe 'Allow PubkeyAuthentication' do
+      it { should contain_ssh_config('PubkeyAuthentication').with('value' => 'no') }
+    end
+  end
+  describe 'Error handling for broken parameter values' do
+    let(:facts) { { :osfamily => 'RedHat' }}
+    describe 'broken protocol' do
+      let (:params) { { :protocol => 'Moo' } }
+      it 'should fail' do
+        should raise_error(Puppet::Error)
+      end
+    end
+    describe 'broken pubkeyauthentication' do
+      let (:params) { { :pubkeyauthentication => 'Moo' } }
+      it 'should fail' do
+        should raise_error(Puppet::Error)
+      end
+    end
+  end
 end
